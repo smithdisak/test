a54ff76b367d878e76d00ff731270f6392365869
smithdisak <39384650+smithdisak@users.noreply.github.com>
2021-02-27 01:32:49 +0000

    Update udac_example_dag.py

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -5,13 +5,13 @@ from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 # from airflow.operators.stage_redshift import StageToRedshiftOperator 
 # 
-# from airflow.operators import ( StageToRedshiftOperator,LoadFactOperator,
-#                                LoadDimensionOperator, DataQualityOperator)
+from airflow.operators import ( StageToRedshiftOperator,LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
 
-from git-plugins.stage_redshift import StageToRedshiftOperator
-from load_fact import LoadFactOperator
-from operators.load_dimension import LoadDimensionOperator
-from operators.data_quality import DataQualityOperator
+# from git-plugins.stage_redshift import StageToRedshiftOperator
+# from load_fact import LoadFactOperator
+# from operators.load_dimension import LoadDimensionOperator
+# from operators.data_quality import DataQualityOperator
 
 from helpers import SqlQueries
 from create_tables import *
